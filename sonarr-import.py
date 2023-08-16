340a5747bd84968feb3045d883d6d0607df0e53f
Davidtober <daveyt36@gmail.com>
2023-08-16 10:53:43 +1000

    Checked actual dates of tasks rather than just counting lines of atq command

--- sonarr-import.py
+++ sonarr-import.py
@@ -3,13 +3,12 @@ import logging
 import os
 import sys
 import subprocess
+from datetime import timedelta, date
 
 logging.basicConfig(filename="/home/pi/Downloads/sonarr-import.log",
                     format="%(asctime)s %(levelname)-8s %(message)s",
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-logging.info("Sonarr extra script post processing started.")
 
 if os.environ.get('sonarr_eventtype') == "Test":
     logging.info("Successful sonarr-import.py SMA test, exiting.")
@@ -25,6 +24,8 @@ try:
 except Exception as error:
     logging.error(f"Error reading environment variables {str(error)}")
     sys.exit(1)
+
+logging.info(f"Processing file: {input_file}")
 try:
     file_components = os.path.splitext(input_file)
     file_extension = file_components[-1]
@@ -78,16 +79,30 @@ bitrate = 8 * file_size / duration
 # Re-encode the video if the bitrate is too high
 if (bitrate / 1000) > target_bitrate:
     logging.info(f"Converting {input_file} from {bitrate/1000}Mb/s -> {conversion_bitrate}Mb/s")
-    status, output = subprocess.getstatusoutput("atq | wc -l")
-    tasks_in_queue = int(output)
-    logging.info(f"{tasks_in_queue} conversion tasks already in queue")
+    _, output = subprocess.getstatusoutput("atq")
+    tasks = output.split("\n")
+    days_to_add = 0
+    formatted_task_dates = [f"{t.split()[3]} {t.split()[2]} {t.split()[5]}" for t in tasks]
+    while True:
+        date_to_check = date.today() + timedelta(days=days_to_add)
+        date_string_to_check = date_to_check.strftime("%-d %b %Y")
+        free = True
+        for task_date in formatted_task_dates:
+            if task_date == date_string_to_check:
+                free = False
+                break
+        if free:
+            break
+        days_to_add += 1
+
+    logging.info(f"Task can be completed in {days_to_add} day(s)")
 
     first_pass = f"ffmpeg -y -i \"{input_file}\" -c:v libx264 -b:v {conversion_bitrate}M -pass 1 -vsync cfr -passlogfile /tmp/dummy_log -f null /dev/null"
     second_pass = f"ffmpeg -i \"{input_file}\" -c:v libx264 -b:v {conversion_bitrate}M -pass 2 -passlogfile /tmp/dummy_log \"{file_root}_converted{file_extension}\""
     conversion_command = f"{first_pass} && {second_pass} && rm \"{input_file}\" && mv \"{file_root}_converted{file_extension}\" \"{input_file}\""
     schedule_command = "at 23:00"
-    if tasks_in_queue > 0:
-        schedule_command = f"at 23:00 + {tasks_in_queue} days"
+    if days_to_add > 0:
+        schedule_command = f"at 23:00 + {days_to_add} days"
     
     full_command = f"echo '{conversion_command}' | {schedule_command}"
     logging.info(f"Running command: {full_command}")
